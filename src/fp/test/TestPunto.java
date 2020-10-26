f5b789496ccd00ad1104f2fb1ae7e675f327465a
Said Mariano <Said Mariano@Said>
2020-10-25 22:15:14 -0600

    TestPunto

---
+++ src/fp/test/TestPunto.java
@@ -0,0 +1,24 @@
+package fp.test;
+
+import fp.punto.*;
+/**
+ *
+ * @author Said Mariano
+ */
+
+public class TestPunto extends Test{
+    
+    public static void main(String[] args) {
+Punto p = new Puntolmpl(2.0,3.0);
+mostrar("Punto:", p);
+p.setX(3.0);
+mostrar("Punto:", p);
+p.setY(2.0);
+mostrar("Punto:", p);
+
+Punto p2 = new Puntolmpl(-1.0, -3.0);
+Double distancia = p.getDistanciaAOtroPunto(p2);
+mostrar("Distancia entre los puntos: ", distancia);
+
+}
+}
